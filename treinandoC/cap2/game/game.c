2ba0de4787b5f65dbaaabc8e10340f0a4a158bd6
richardtjustke <rj.justke@gmail.com>
2026-03-31 20:06:30 -0300

    game

--- treinandoC/cap2/game/game.c
+++ treinandoC/cap2/game/game.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+
+int main(void) {
+  int segredo = 5;
+  int palpite;
+
+  while (segredo != palpite) {
+    printf("Digite um número de 0 a 10:");
+
+    scanf("%d", &palpite);
+
+    if (segredo >= palpite) {
+      printf("um pouco mais");
+    } else if (segredo <= palpite) {
+      printf("um pouco menos");
+    }
+  }
+  printf("Vc acertou!!!");
+}
