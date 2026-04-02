293f3b6716514d1fa7145d386368ef9a72625abf
RichardtJustke <rj.justke@gmail.com>
2026-04-02 10:17:14 -0300

    tcc

---
+++ treinandoC/cap2/caixa eletronico/cashpay.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+  int carteira = 1000;
+  int opcao;
+  int cofn;
+  int valor;
+  while () {
+    printf("1- Sacar");
+    printf("2- Depositar");
+    printf("3- Ver Saldo");
+    printf("4- Sair");
+    printf("Digite sua opção:");
+    scanf("%d", &opcao);
+
+    if (opcao == 0) {
+      printf("Você saiu do sistema!");
+      return 0;
+    } else if (opcao == 1) {
+      printf("Qual valor vc quer sacar:");
+      scanf("%d", &valor);
+      printf()
+    } else if (opcao == 2) {
+      printf("");
+
+    } else if (opcao == 3) {
+      printf("");
+    }
+  }
+}
